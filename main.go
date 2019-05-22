daa42f14b03b02ac98e47124f1751902b427d0b1
Stephen Schlie <heschlie@gmail.com>
2019-05-21 19:28:07 -0700

    Adding in aws-sdk, starting adventure into madness

--- main.go
+++ main.go
@@ -4,40 +4,82 @@ import (
 	"context"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/autoscaling"
+	"github.com/aws/aws-sdk-go/service/servicediscovery"
+	"github.com/aws/aws-sdk-go/service/ec2"
 	"log"
+	"strings"
+	"time"
 )
 
-const TAG = "plos/service"
+const TAG_PREFIX = "plos/"
 
 func Handler(ctx context.Context, event events.AutoScalingEvent) {
 	asgName := event.Detail["AutoScalingGroupName"].(string)
 	asgArn := event.Resources[0]
-	ec2Arn := event.Resources[1]
+	ec2Id := event.Detail["EC2InstanceId"].(string)
 
 	asgFilter := autoscaling.DescribeAutoScalingGroupsInput{
 		AutoScalingGroupNames: []*string{ &asgName },
 	}
+
 	sess := session.Must(session.NewSession())
 	asgSession := autoscaling.New(sess)
+	sdSession := servicediscovery.New(sess)
+	ec2Session := ec2.New(sess)
 
-	response, err := asgSession.DescribeAutoScalingGroups(&asgFilter)
+	asgResponse, err := asgSession.DescribeAutoScalingGroups(&asgFilter)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	var services []string
-	for _, asg := range response.AutoScalingGroups {
+	var services map[string]string
+	for _, asg := range asgResponse.AutoScalingGroups {
 		if *asg.AutoScalingGroupARN == asgArn {
 			for _, tag := range asg.Tags {
-				if *tag.Key == TAG {
-					services = append(services, *tag.Value)
+				if strings.HasPrefix(*tag.Key, TAG_PREFIX) {
+
 				}
 			}
 		}
 	}
 
+	ec2Filter := ec2.DescribeInstancesInput{
+		InstanceIds: []*string{ &ec2Id },
+	}
+
+	ec2Response, err := ec2Session.DescribeInstances(&ec2Filter)
+	if err != nil {
+		log.Fatal(err)
+	}
+	ec2Ip := ec2Response.Reservations[0].Instances[0].PrivateIpAddress
+
+}
+
+func registerService(sd *servicediscovery.ServiceDiscovery, service string, instance string, privateIp *string) {
+
+	serviceFilter := servicediscovery.ServiceFilter{
+		Condition: aws.String(servicediscovery.FilterConditionEq),
+		Name: aws.String(servicediscovery.OperationFilterNameNamespaceId),
+		Values: aws.StringSlice([]string{ })
+
+	}
+	serviceInput := servicediscovery.ListServicesInput{
+
+	}
+
+	sd.ListServices()
+
+	input := servicediscovery.RegisterInstanceInput{
+		Attributes: map[string]*string{ "AWS_INSTANCE_IPV4": privateIp },
+		CreatorRequestId: aws.String(time.Now().String()),
+	}
+
+}
+
+func unregisterService(sd *servicediscovery.ServiceDiscovery, service string, instance string) {
 
 }
 
